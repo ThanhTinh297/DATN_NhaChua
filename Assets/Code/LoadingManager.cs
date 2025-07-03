56daf620a9306f136030c4d8c45a25b8e66cd1c4
Bao41nsw0rth <ltqb7122005@gmail.com>
2025-07-03 11:50:43 +0700

    LoadingScene(OnlyProgressBar)

---
+++ Assets/Code/LoadingManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadingManager : MonoBehaviour
+{
+    public Image fillImage; // Image có Image Type = Filled
+    public float fillSpeed = 3f;
+
+    private float targetProgress = 0f;
+
+    void Start()
+    {
+        StartCoroutine(LoadAsyncScene());
+    }
+
+    void Update()
+    {
+        // Tăng dần fillAmount về phía targetProgress
+        if (fillImage != null)
+        {
+            fillImage.fillAmount = Mathf.Lerp(fillImage.fillAmount, targetProgress, Time.deltaTime * fillSpeed);
+        }
+    }
+
+    IEnumerator LoadAsyncScene()
+    {
+
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync("MainScene");
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            // Chuyển progress 0 → 1 mượt mà
+            targetProgress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            // Khi scene load gần xong
+            if (operation.progress >= 0.9f && fillImage.fillAmount >= 0.995f)
+            {
+                yield return new WaitForSeconds(0.5f);;
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+
+
+}
