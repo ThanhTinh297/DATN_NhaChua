409870358222607e6f329ef4deac14c39481cfd8
Went <bavuong765@gmail.com>
2025-08-07 21:46:03 +0700

    skip
    UP thêm cái skip

---
+++ Assets/Skip.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    // Gán GameObject cần xóa trong Inspector
+    public GameObject objectToDestroy;
+
+    void Update()
+    {
+        // Kiểm tra nếu phím P được nhấn
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            // Xóa GameObject nếu nó tồn tại
+            if (objectToDestroy != null)
+            {
+                Destroy(objectToDestroy);
+            }
+        }
+    }
+}
