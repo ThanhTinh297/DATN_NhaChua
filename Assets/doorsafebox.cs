2a2343e65687784ac97bb7e990b1aaf79bbd858c
Went <bavuong765@gmail.com>
2025-08-22 14:19:44 +0700

    dell bt t dag lm cai dell j

---
+++ Assets/doorsafebox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorsafebox : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        // Lấy component Rigidbody của object
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Kiểm tra nếu va chạm với object có tên "tad key"
+        if (collision.gameObject.name == "keysafe")
+        {
+            // Tắt IsKinematic
+            if (rb != null)
+            {
+                rb.isKinematic = false;
+            }
+        }
+    }
+}
