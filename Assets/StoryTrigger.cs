ece0e8ba779851d20569b0897bb15203063c5ceb
Went <bavuong765@gmail.com>
2025-08-24 14:49:02 +0700

    storytrigger  để khi mà người chơi đi vào một vị trí nào đó thì sẽ hiện story lên

---
+++ Assets/StoryTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToActivate; // GameObject cần kích hoạt
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra nếu đối tượng va chạm có tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Kích hoạt GameObject nếu nó tồn tại
+            if (objectToActivate != null)
+            {
+                objectToActivate.SetActive(true);
+            }
+            // Phá hủy GameObject chứa script này
+            Destroy(gameObject);
+        }
+    }
+}
+}
